0092ab94e3f4aa5be8f5782b4056bd63a2094642
Michael Woolley <mwoolley2@gmail.com>
2024-06-16 13:43:28 -0400

    Added material package and added material toggle component as test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { GoogleMap } from '@angular/google-maps';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, GoogleMap, MatSlideToggleModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'google-maps';
+  options: google.maps.MapOptions = {
+    mapId: 'DEMO_MAP_ID',
+    center: { lat: -31, lng: 147 },
+    zoom: 4,
+  };
 }
